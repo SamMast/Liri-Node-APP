b5eb033884de110a61ae471bd41c81263d2e43fd
Samuel Mast <samuel.a.mast@gmail.com>
2018-02-13 16:59:51 -0700

    app works properly, but still working on write log function

--- liri.js
+++ liri.js
@@ -1,9 +1,113 @@
-// require("dotenv").config();
+require("dotenv").config();
 
-// var spotify = new Spotify(keys.spotify);
-// var client = new Twitter(keys.twitter);
+const Twitter = require('twitter');
+const Spotify = require('node-spotify-api');
+
+const twitterKeys = require('./keys.js').twitter;
+const spotifyKeys = require('./keys.js').spotify;
+
+
+const client = new Twitter(twitterKeys);
+const spotify = new Spotify(spotifyKeys);
 
 var action = process.argv[2];
+var newValue = "";
+
+function tweetSearch() {
+	
+	var params = {screen_name: 'sleechinator'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  	if (!error) {
+		  	for (var i = 0; i < 20; i++) {
+				console.log("-------")
+		    	console.log(`Tweet ${i+1}: ${tweets[i].text}`);
+		    	console.log(`Timestamp: ${tweets[i].created_at}`)
+			}
+	  	}
+	});
+}
+
+function spotifySearch(item) {
+	
+	var search = (item || process.argv[3] || "The Sign, Ace of Base")
+
+	if (process.argv[4]) {
+		for (var i = 4; i < process.argv.length; i++) {
+			search += (`+${process.argv[i]}`)
+		}
+	}
+
+	spotify.search({ type: 'track', query: search }, function(err, data) {
+	  	if (err) {
+	    	return console.log('Error occurred: ' + err);
+	  	}
+
+		console.log("-------") 
+		console.log(data.tracks.items[0].album.artists[0].name); 
+		console.log(data.tracks.items[0].name); 
+		console.log(data.tracks.items[0].preview_url);
+		console.log(data.tracks.items[0].album.name); 
+		console.log("-------")
+
+
+	});	
+}
+
+function movieSearch(item) {
+	
+	var request = require("request");
+
+	if (process.argv[3]) {
+
+		var movieName = (process.argv[3]);
+
+		for (var i = 4; i < process.argv.length; i++) {
+		movieName += `+${process.argv[i]}`;
+		}
+
+	} else {
+
+		var movieName = (item || "Mr. Nobody")
+	}
+
+	var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+	request(queryUrl, function(error, response, body) {
+
+		if (!error && response.statusCode === 200) {
+			console.log("-------------")
+	    	console.log(`Title: ${JSON.parse(body).Title}`);
+	    	console.log(`Release Year: ${JSON.parse(body).Year}`);
+	    	console.log(`IMBD Rating: ${JSON.parse(body).imdbRating}/10`);
+	    	console.log(`Rotten Tomatoes Rating: ${JSON.parse(body).Ratings[1].Value}`);	    	
+	    	console.log(`Country: ${JSON.parse(body).Country}`);
+	    	console.log(`Language: ${JSON.parse(body).Language}`);
+	    	console.log(`Plot: ${JSON.parse(body).Plot}`);
+ 	    	console.log(`Actors: ${JSON.parse(body).Actors}`);
+			console.log("-------------")
+	  	}
+
+	});	
+}
+
+function decide() {
+
+	if (action === "my-tweets") {
+	
+		tweetSearch(newValue);
+
+	} else if (action === "spotify-this-song") {
+
+		spotifySearch(newValue);
+
+	} else if (action === "movie-this") {
+		
+		movieSearch(newValue);
+
+	}
+
+}
+
 
 function writeLog() {
 
@@ -28,60 +132,8 @@ function writeLog() {
 
 
 //Conditionals
-if (action === "my-tweets") {
-//This will show your last 20 tweets and when they were created at in your terminal/bash window.
 
-
-
-
-
-
-} else if (action === "spotify-this-song") {
-
-// This will show the following information about the song in your terminal/bash window
-	// Artist(s)
-	// The song's name
-	// A preview link of the song from Spotify
-	// The album that the song is from
-	// If no song is provided then your program will default to "The Sign" by Ace of Base.
-
-} else if (action === "movie-this") {
-	
-	var request = require("request");
-
-	if (process.argv[3]) {
-
-		var movieName = process.argv[3];
-
-		for (var i = 4; i < process.argv.length; i++) {
-		movieName += `+${process.argv[i]}`;
-		}
-
-	} else {
-
-		var movieName = "Mr. Nobody"
-	}
-
-	var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-
-	console.log(queryUrl);
-
-	request(queryUrl, function(error, response, body) {
-
-		if (!error && response.statusCode === 200) {
-	    	console.log(`Title: ${JSON.parse(body).Title}`);
-	    	console.log(`Release Year: ${JSON.parse(body).Year}`);
-	    	console.log(`IMBD Rating: ${JSON.parse(body).imdbRating}/10`);
-	    	console.log(`Rotten Tomatoes Rating: ${JSON.parse(body).Ratings[1].Value}`);	    	
-	    	console.log(`Country: ${JSON.parse(body).Country}`);
-	    	console.log(`Language: ${JSON.parse(body).Language}`);
-	    	console.log(`Plot: ${JSON.parse(body).Plot}`);
- 	    	console.log(`Actors: ${JSON.parse(body).Actors}`);
-	  	}
-
-	});
-
-} else if (action === "do-what-it-says") {
+if (action === "do-what-it-says") {
 
 	var fs = require("fs");
 
@@ -91,16 +143,18 @@ if (action === "my-tweets") {
 	    	return console.log(error);
 	  	}
 
-	  	console.log(data);
-
 	  	var dataArr = data.split(",");
-	  	var newAction = dataArr[0];
-	  	var newValue = dataArr[1];
-	  	console.log(newAction);
+	  	action = dataArr[0];
+	  	newValue = dataArr[1];
+	  	console.log(action);
 	  	console.log(newValue);
-
-	  	//now make it do what it says in the new action with the new value
+	  	decide();
 
 	});
 
+} else {
+	decide();
 }
+
+
+
